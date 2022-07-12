dbd799d99a1ad1e3f2051472994b71e976253189
Anshul Tripathi <tripathianshul06@gmail.com>
2022-07-12 16:02:58 +0530

    Jwt

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ require("./db/conn");
 const bcrypt = require("bcryptjs");
 
 
+
 const staticpath = path.join(__dirname, "../public")
 
 // app.use(express.static(staticpath))
@@ -13,18 +14,50 @@ const staticpath = path.join(__dirname, "../public")
 app.set("view engine", "hbs")
 
 
-app.get("/", (req, res) => {
+app.get("/", (req, res)=>{
     res.render("index")
 });
 
 
- 
+
+
+// create a new user in our database
+// app.post(" / register ", async (req, res) => {
+//     try {
+//         const password = req.body.password;
+//         const cpassword = req.body.confirmpassword;
+//         if (password == cpassword) {
+//             const registerEmployee = new Register({
+//                 firstname: req.body.firstname,
+//                 lastname: req.body.Lastname,
+//                 email: req.body.email,
+//                 gender: req.body.gender,
+//                 phone: req.body.phone,
+//                 age: req.body.age,
+//                 password: password,
+//                 confirmpassword: cpassword
+//             })
+
+                // const token = await registerEmployee.generateAuthToken();
+//             const registered = await registerEmployee.save();
+//             res.status(201).render(" index ");
+//         } else {
+//             res.send(" password are not matching ")
+//         }
+//     }
+//     catch (error) {
+//         res.status(400).send(error);
+//     }
+// })
+
+
+
 app.post("/login", async (req, res) => {                                        // for login
     try {
         const email = req.body.email;
         const password = req.body.password;
         const useremail = await Register.findOne({ email: email });
-        if (useremail.password = password) {
+        if (useremail.password == password) {
             res.status(201).render("index");
         } else {
             res.send("password are not matching")
@@ -48,6 +81,43 @@ const securepassword = async (password) => {
 
 securepassword("anshul");
 
+
+//first
+
+const jwt = require("jsonwebtoken");
+const { registerPartial } = require("hbs");
+const createToken = async () => {
+    const token = await jwt.sign({ _id: "dbkey" }, "secretkey",{expiresIn:"2 seconds"});  //token generation
+    console.log(token);
+    const userver = await jwt.verify(token, "secretkey");
+    console.log(userver);    //it returns id and issued at
+}
+createToken();
+
+
+//the res.cookie() function is used to set the cookie name to value.the value parameter may be a string or object converted to json
+
+// res.cookie("jwt",token)        //token stored in cookie
+// console.log(cookie);
+
+//three parametrs of res.cookie httpOnly-true-client side scripting lang cookies ko cheed nhi skta.Expiration bhi kra skte h
+
+//cookie parser is used to fetch data from cookies
+//req.cookies.jwt  //jwt is cookie name
+
+
+app.get("/logout", auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((currElement) => {
+            return currElement.token = req.token
+        })
+        res.clearCookie("jwt");
+        await req.user.save();
+        res.render("login");
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
 
 
 app.listen(port, () => {
--- src/models/schema.js
+++ src/models/schema.js
@@ -1,15 +1,43 @@
 // everything bw two components is middleware.
 
+
+// tokens:[{
+//     token:{
+//         type:String,
+//         required:true
+//     }
+// }]
+
 // schema has a function called as pre
-const bcrypt=require("bcryptjs")
+const bcrypt = require("bcryptjs")
 
 
-employeeSchema.pre ( "save" , async function (next) {      // it is a function which runs before save and hashes the password
-    if ( this.isModified ( "password" ) ) {
-    console.log ( ` the current password is ${this.password}`) ;
-    this.password = await bcrypt.hash ( this.password , 10 ) ;
-    console.log ( ` the current password is ${this.password}` ) ;
+// generating tokens
+employeeSchema.methods.generateAuthToken = async function () {
+    try {
+        const token = jwt.sign({ _id: this._id.toString()}, "mynameisvinodbahadurthapayoutuber");
+        console.log(token);
+        this.tokens = this.tokens.concat({token:token})
+        return token;
     }
-    this.confirmpassword=undefined;            //isey store hi mt kro
-    next() ;
-    } )
+    catch (error) {
+        res.send("the error part" + error);
+        console.log("the error part" + error);
+    }
+}
+
+employeeSchema.pre("save", async function (next) {      // it is a function which runs before save and hashes the password
+    if (this.isModified("password")) {
+        console.log(` the current password is ${this.password}`);
+        this.password = await bcrypt.hash(this.password, 10);
+        console.log(` the current password is ${this.password}`);
+    }
+    this.confirmpassword = undefined;            //isey store hi mt kro
+    next();
+})
+
+
+// statics are the methods defined on models
+// methods are defined on the documents
+// use .statics for static methods
+// use .methods for instance methods
